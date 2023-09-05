7720d1c4cecfadc9123fcfefa41f1e56fb6a6ffe
Eric P Green <ericpgreen2@gmail.com>
2023-09-05 12:53:10 -0400

    Add homepage w/ links

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  const routes = [
+    {
+      name: "Dialog",
+      path: "/dialog",
+    },
+    {
+      name: "Popover",
+      path: "/popover",
+    },
+    {
+      name: "Tabs",
+      path: "/tabs",
+    },
+  ];
+</script>
+
+<nav>
+  <ul>
+    {#each routes as route}
+      <li>
+        <a href={route.path}>{route.name}</a>
+      </li>
+    {/each}
+  </ul>
+</nav>
